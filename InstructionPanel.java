e5056cba7ef4a8a2a4c44fb2c617011b83dd8cc8
trrt-good <t.atwood.rose@gmail.com>
2022-04-19 13:26:45 -0700

    camera changes and general cleanup

--- InstructionPanel.java
+++ InstructionPanel.java
@@ -1,39 +1,18 @@
 import java.awt.CardLayout;
-import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JFrame;    
+import java.awt.BorderLayout;   
 import javax.swing.JPanel;
-
-import javax.swing.JButton;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.JCheckBox;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
 import javax.swing.JTextArea;
-import javax.swing.JLabel;
-import javax.swing.JSlider;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
+
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.Insets;
 import java.util.ArrayList;
 
-import java.awt.Image;
 import java.io.File;
-import javax.imageio.ImageIO;
-import java.io.IOException;
 
 import java.util.Scanner;
 import java.io.FileNotFoundException;
